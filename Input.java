a8c30305e5708e71012be98f3eb165850e0cc353
Priyadarshini7772 <161844172+Priyadarshini7772@users.noreply.github.com>
2024-04-24 19:45:00 +0530

    Update Input.java

--- Input.java
+++ Input.java
@@ -6,11 +6,10 @@ public class Input {
     public static void main(String[] args) {
             System.out.print("Enter ur name: ");
             Scanner sc = new Scanner(System.in) ;
-            String name=sc.next();
+            String name=sc.nextLine();
             System.out.println(name);
             System.out.print("Enter your full name: ");
-            Scanner sc1 = new Scanner(System.in) ;
-            String name1=sc1.nextLine();
+            String name1=sc.nextLine();
             System.out.println(name1);
         
     }
