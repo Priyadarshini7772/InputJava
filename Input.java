94062fa03df9a53c35b5d23f132a8ab96cdb5b94
Abhi Priya Darshini Kurasa <abhipriyadarshinikurasa@gmail.com>
2024-04-24 19:07:11 +0530

    UST

---
+++ Input.java
@@ -0,0 +1,17 @@
+package InputJava;
+import java.util.Scanner;
+
+public class Input {
+
+    public static void main(String[] args) {
+System.out.print("Enter ur name: ");
+ Scanner sc = new Scanner(System.in) ;
+             String name=sc.next();
+            System.out.println(name);
+            System.out.print("Enter your full name: ");
+            Scanner sc1=new Scanner(System.in);
+            String name1=sc1.nextLine();
+            System.out.println(name1);
+        
+    }
+}
