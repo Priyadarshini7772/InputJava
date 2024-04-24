e4f0246aaf80c260860e16ccb5087346e0154fbc
Abhi Priya Darshini Kurasa <abhipriyadarshinikurasa@gmail.com>
2024-04-24 19:30:32 +0530

    calculator

---
+++ calculator.java
@@ -0,0 +1,39 @@
+package InputJava;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        while (true){
+        System.out.print("num1: ");
+        Scanner sc =new Scanner(System.in);
+        int num1=sc.nextInt();
+
+        System.out.print("num2: ");
+        Scanner sc1=new Scanner(System.in);
+        int num2=sc1.nextInt();
+
+        System.out.print("Enter your choice(+,-,*,/,%): ");
+        Scanner sc2=new Scanner(System.in);
+        String ch=sc2.next();
+        int i=0;
+        switch(ch){
+            case "+": System.out.println("Result: "+(num1+num2));
+             break;
+            case "-": System.out.println("Result: "+(num1-num2));
+             break;
+            case "*":System.out.println("Result: "+(num1*num2));
+             break;
+            case "/":System.out.println("Result: "+((float)num1/num2));
+             break;
+            case "%": System.out.println("Result: "+(num1%num2));
+             break;
+            default:
+            System.out.println("Invalid operation");
+            i=1;
+        }
+        if (i==1){
+            break;
+        }
+    }}
+}
