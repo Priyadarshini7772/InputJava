ee733569ce99ba6f4dcf3213927d8f2ea6df433a
Abhi Priya Darshini Kurasa <abhipriyadarshinikurasa@gmail.com>
2024-04-25 19:05:55 +0530

    ArraysImplemrntation

---
+++ ArraysImplem.java
@@ -0,0 +1,18 @@
+package InputJava;
+
+import java.util.Arrays;
+
+import javax.sound.midi.Soundbank;
+
+public class ArraysImplem {
+    public static void main(String[] args) {
+        String[] students= new String[8];
+        students[0]="Abhi";
+        //OR
+        int[] numbers= {1,2,3};
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(students));
+        
+    }
+    
+}
